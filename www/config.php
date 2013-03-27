2ccaf22d75de98dfe7cc3f547f9f19d0a2a33f97
denghf <hfdend@gmail.com>
2013-03-27 14:23:34 +0800

    css

--- www/config.php
+++ www/config.php
@@ -2,4 +2,40 @@
 
 class Config extends Conf
 {
+	/**
+     * mysql主机地址
+     */
+    const DB_MYSQL_HOST = '127.0.0.1';
+    
+    /**
+     * mysql 用户名称
+     */
+    const DB_MYSQL_USERNAME = 'root';
+    
+    /**
+     * mysql 密码
+     */
+    const DB_MYSQL_PADDWORD = 'llpp@quzhao.com,./123';
+    
+    /**
+     * 数据库名称
+     */
+	const DB_MYSQL_DBNAME = 'db_quzhao';
+
+	/**
+	 * redis 地址
+	 */
+	const REDIS_HOST = '127.0.0.1';
+
+	/**
+	 * redis 端口
+	 */
+	const REDIS_PORT = '6379';
+
+	/**
+	 * redis 密码
+	 */
+	const REDIS_PASSWORD = '#$@quZhao';
+ 
+
 }
