6cf006ae02ef0f356dfc902aeccb1ca36ce23e9b
denghf <hfdend@gmail.com>
2013-03-27 11:39:06 +0800

    msg

--- www/view/dp/index.php
+++ www/view/dp/index.php
@@ -21,6 +21,36 @@ class IndexDpView extends BaseView
 		$this->assign('tjModels', $tjModels);
 		$this->assign('dpBrowseHistoryModels', $dpBrowseHistoryModels);
         $this->assign('pageCore',$pageCore);
+		$editorArr = array(
+			'珞邪',
+			'夏天爱清新',
+			'TomaTo迷离 ',
+			'生如夏花之灿烂 ',
+			'McQueen的時光記事本',
+			'桃子_miko',
+			'小怪兽s',
+			'妙妙辛',
+			'没囿紅色的紅色', 
+			'Miss_mario',
+			'一个太阳一朶向日葵',
+			'小小漫m丶',
+			'LNP蓝风',
+			'袋鼠康康',
+			'斯图彼得猫',
+			'天魔霸邪',
+			'唐朝秀才郎',
+			'木木-lol',
+			'黑夜里的网子',
+			'小家伙的足迹',
+			'尐黑妞Serena',
+			'雨后巴黎小小温情', 
+			'喊我33就很好',
+			'泡芙小米粒',
+			'皮蛋兔肉粥',
+			'单小V_V',
+			'糊大人cocoa',
+		);
+		$this->assign('editorArr', $editorArr);
 	}
 	
 	private function dpBrowseHistoryModels()
