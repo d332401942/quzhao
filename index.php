8aabe6402706ab68e7dd9bc72a170846a048886e
denghf <hfdend@gmail.com>
2013-04-01 11:17:16 +0800

    Signed-off-by: denghf <hfdend@gmail.com>

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
+
 define('APP_DIR', './www');
-define('APP_ROOT','');
-define('APP_URL','');
+define('APP_ROOT', '');
+define('APP_URL', '');
 
 include '../feng/main.php';
 include './request.php';
 $regulation = array(
-	'/^\/?ajax(.*?)__(.*)/' => array('ajaxRequest', 1, 2),
-	'/^\/?ajax(.*)/' => array('ajaxRequest', 1),
-	'/(.*?)__(.*)/' => array('httpRequest', 1, 2),
-	'/.*/' => array('httpRequest', 0),
+    '/^\/?ajax(.*?)__(.*)/' => array('ajaxRequest', 1, 2),
+    '/^\/?ajax(.*)/' => array('ajaxRequest', 1),
+    '/(.*?)__(.*)/' => array('httpRequest', 1, 2),
+    '/.*/' => array('httpRequest', 0),
 );
 $mainClass = new Main();
 $notFoundFileName = 'www/404.html';
