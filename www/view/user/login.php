0072365979b1c02951320753d20c0fe9a97c1054
denghf <hfdend@gmail.com>
2013-04-24 15:45:07 +0800

    msg

--- www/view/user/login.php
+++ www/view/user/login.php
@@ -5,6 +5,7 @@ class LoginUserView extends BaseView
 	
 	public function index()
 	{
+		$this->responseError('');
 		$this->setMeta();
 		$connectModel = new ConnectModel($this->ServerName ,'http://' .$this->ServerName);
 		$this->assign('connectModel', $connectModel);
