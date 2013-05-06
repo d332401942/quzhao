54a5c250a2607ec486000d2eed59a62b90194b18
denghf <hfdend@gmail.com>
2013-05-06 19:34:01 +0800

    msg

--- admin/config.php
+++ admin/config.php
@@ -5,7 +5,7 @@ class Config extends Conf
 	/**
      * mysql主机地址
      */
-    const DB_MYSQL_HOST = '192.168.1.126';
+    const DB_MYSQL_HOST = '127.0.0.1';
     
     /**
      * mysql 用户名称
@@ -25,7 +25,7 @@ class Config extends Conf
 	/**
 	 * redis 地址
 	 */
-	const REDIS_HOST = '192.168.1.126';
+	const REDIS_HOST = '127.0.0.1';
 
 	/**
 	 * redis 端口
