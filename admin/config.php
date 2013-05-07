968531972fb1da7da9828c5deb9b7eb7c6f5af63
wangbingang <wangbingang@quzhao.com>
2013-05-07 15:36:47 +0800

    x

--- admin/config.php
+++ admin/config.php
@@ -6,6 +6,20 @@ class Config extends Conf
      * mysql主机地址
      */
     const DB_MYSQL_HOST = '192.168.1.126';
+	
+	/**
+	 * 搜索数据库名称
+	 */
+	const DB_MYSQL_SEARCH_DBNAME = 'db_quzhao_search';
+	/**
+	 * mysql 搜索主机地址
+	 */
+	const DB_MYSQL_SEARCH_HOST = '114.112.163.2';
+
+	/**
+	 * mysql 搜索主机端口
+	 */
+	const DB_MYSQL_SEARCH_PORT = 9350;
     
     /**
      * mysql 用户名称
@@ -16,6 +30,10 @@ class Config extends Conf
      * mysql 密码
      */
     const DB_MYSQL_PADDWORD = 'llpp@quzhao.com,./123';
+	 /**
+     * mysql 密码
+     */
+    const DB_MYSQL_PASSWORD = 'llpp@quzhao.com,./123';
     
     /**
      * 数据库名称
