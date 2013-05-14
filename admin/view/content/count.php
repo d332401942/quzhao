7c36bf6da281114ef5ac1b249443cd71c8208fde
wangbingang <wangbingang@quzhao.com>
2013-05-14 11:03:11 +0800

    x

--- admin/view/content/count.php
+++ admin/view/content/count.php
@@ -11,19 +11,19 @@ class CountContentView extends BaseView
 		}
 		
 		$strtime = null;
-		if(!empty($_GET['strtime']))
+		if(!empty($_GET['time_start']))
 		{
-			$strtime = $_GET['strtime'];
+			$strtime = $_GET['time_start'];
 		}
 		
 		$endtime = null;
-		if(!empty($_GET['endtime']))
+		if(!empty($_GET['time_end']))
 		{
-			$endtime = $_GET['endtime'];
+			$endtime = $_GET['time_end'];
 		}
 		
 		/*
-		 *得到今天总数据
+		 *得到数据数目
 		 */
 		$business = new HomeTjDataBusiness();
 		$totalNum = $business->getDayNumber($cate, $strtime, $endtime, 0);
@@ -39,25 +39,33 @@ class CountContentView extends BaseView
 			{
 				if($total['id'] == $numT['id'])
 				{
-					$totalNum[$key]['num2'] = $numT['num2']; 
+					$totalNum[$key]['numTrue'] = $numT['num']; 
 				}
 				else
 				{
-					$totalNum[$key]['num2'] = 0; 
+					$totalNum[$key]['numTrue'] = 0; 
 				}
 			}
 			foreach($totalNumFalse as $numF)
 			{
 				if($total['id'] == $numF['id'])
 				{
-					$totalNum[$key]['num3'] = $numF['num3']; 
+					$totalNum[$key]['numFalse'] = $numF['num']; 
 				}
 				else
 				{
-					$totalNum[$key]['num3'] = 0;
+					$totalNum[$key]['numFalse'] = 0;
 				}
 			}
 		}
+		/**
+		 * 日期插件
+		 */
+		$dateFrom = FormCommon::date('time_start',date('Y-m-d H:i:s'),true);
+		$dateTo = FormCommon::date('time_end',date('Y-m-d H:i:s'),true);
+		$this->assign('dateTo', $dateTo);
+		$this->assign('dateFrom', $dateFrom);
+		
 		$this->assign('totalNum', $totalNum);
 		$this->assign('title', '商品统计');
 	}
